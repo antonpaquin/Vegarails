5722863472160c39166197dbb09288157098d150
anton <antonpaquin@gmail.com>
2016-04-12 19:32:53 +0000

    Now home works from a DB!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def show
-    @services = {:self => ['Manga', 'Webcomics', 'Anime', 'Music', 'FTP'], :third => ['Deluge', 'Plex']}
+    @services = {
+      :self => Service.where(category: 0).all.map {|s| s.name},
+      :third => Service.where(category: 1).all.map {|s| s.name}}
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160412190645) do
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
