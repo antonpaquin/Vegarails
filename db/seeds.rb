21e5349350859ac52bbee72afa38629e7f79089d
anton <antonpaquin@gmail.com>
2016-04-17 12:46:23 +0000

    Anime index looks like it will look now

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,24 +27,24 @@ anime = [
 ]
 
 def plexform(name, ep, season)
-  '/Media/Anime/' + name + '/Season 0' + season + '/' + name + ' - s0' + season + 'e0' + ep + '.mkv'
+  '/Media/Anime/' + name + '/Season ' + season.to_s.rjust(2,'0') + '/' + name + ' - s' + season.to_s.rjust(2,'0') + 'e' + ep.to_s.rjust(2,'0') + '.mkv'
 end
 anime_episodes = [
 #  Name                                Anime_id   Length   Season   Episode  Watched  File
-  ['Time for Bear and Girl to Part',   1,         '25:00', 1,       1,       true,    plexform('Kuma Miko', '1', '1')    ],
-  ['A Hard Road',                      1,         '25:00', 1,       1,       false,   plexform('Kuma Miko', '2', '1')    ],
-  ['Kanna, the Cutting Sword',         2,         '0',     1,       1,       true,    plexform('Katanagatari', '1', '1') ],
-  ['Namakura, the Decapitation Sword', 2,         '0',     1,       2,       true,    plexform('Katanagatari', '2', '1') ],
-  ['Tsurugi, the Sword of Thousand',   2,         '0',     1,       3,       true,    plexform('Katanagatari', '3', '1') ],
-  ['Hari, the Slender Sword',          2,         '0',     1,       4,       true,    plexform('Katanagatari', '4', '1') ],
-  ['Yoroi, the Rebel Sword',           2,         '0',     1,       5,       true,    plexform('Katanagatari', '5', '1') ],
-  ['Kanazuchi, the Twin Swords',       2,         '0',     1,       6,       true,    plexform('Katanagatari', '6', '1') ],
-  ['Bita, the Evil Sword',             2,         '0',     1,       7,       true,    plexform('Katanagatari', '7', '1') ],
-  ['Sai, the Minute',                  2,         '0',     1,       8,       true,    plexform('Katanagatari', '8', '1') ],
-  ['Nokogiri, the Sword of Kings',     2,         '0',     1,       9,       true,    plexform('Katanagatari', '9', '1') ],
-  ['Hakari, the Sword of Truth',       2,         '0',     1,       10,      true,    plexform('Katanagatari', '10', '1')],
-  ['Mekki, the Poison Sword',          2,         '0',     1,       11,      true,    plexform('Katanagatari', '11', '1')],
-  ['Juu, the Flame Sword',             2,         '0',     1,       12,      true,    plexform('Katanagatari', '12', '1')]
+  ['Time for Bear and Girl to Part',   1,         '25:00', 1,       1,       true,    plexform('Kuma Miko', 1, 1)    ],
+  ['A Hard Road',                      1,         '25:00', 1,       2,       false,   plexform('Kuma Miko', 2, 1)    ],
+  ['Kanna, the Cutting Sword',         2,         '0',     1,       1,       true,    plexform('Katanagatari', 1, 1) ],
+  ['Namakura, the Decapitation Sword', 2,         '0',     1,       2,       true,    plexform('Katanagatari', 2, 1) ],
+  ['Tsurugi, the Sword of Thousand',   2,         '0',     1,       3,       true,    plexform('Katanagatari', 3, 1) ],
+  ['Hari, the Slender Sword',          2,         '0',     1,       4,       true,    plexform('Katanagatari', 4, 1) ],
+  ['Yoroi, the Rebel Sword',           2,         '0',     1,       5,       true,    plexform('Katanagatari', 5, 1) ],
+  ['Kanazuchi, the Twin Swords',       2,         '0',     1,       6,       true,    plexform('Katanagatari', 6, 1) ],
+  ['Bita, the Evil Sword',             2,         '0',     1,       7,       true,    plexform('Katanagatari', 7, 1) ],
+  ['Sai, the Minute',                  2,         '0',     1,       8,       true,    plexform('Katanagatari', 8, 1) ],
+  ['Nokogiri, the Sword of Kings',     2,         '0',     1,       9,       true,    plexform('Katanagatari', 9, 1) ],
+  ['Hakari, the Sword of Truth',       2,         '0',     1,       10,      true,    plexform('Katanagatari', 10, 1)],
+  ['Mekki, the Poison Sword',          2,         '0',     1,       11,      true,    plexform('Katanagatari', 11, 1)],
+  ['Juu, the Flame Sword',             2,         '0',     1,       12,      true,    plexform('Katanagatari', 12, 1)]
 ]
 
 services.each do |name, category|
